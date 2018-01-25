5d590fb904d544554a9f3ffbebef810750431498
CodyGividen <codygividen5@gmail.com>
2018-01-25 15:01:02 -0500

    Finshed project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,26 @@ public class Main {
 
 	    if(a == x && y + y == a){
 	        System.out.println("a == x && y + y == a");
-        }
+
+
+	        /*
+	            && - AND - both conditions have to be true for this to return true
+	            || - OR - only one has to be true
+	            ! - NOT - !(condition)
+	            == - equal to - validates condition
+	         */
+
+
+
+			if(a == x && y + y == a){
+				System.out.println("a == x && y + y == a");
+
+			if (a < b){
+				System.out.println('a < b');
+			}
+
+
+			}
+
     }
 }
